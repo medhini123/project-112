3f4ca1e2b458d2f91056b1ff6c157891f6f14fd8
medhini123 <107126947+medhini123@users.noreply.github.com>
2023-08-14 17:16:41 +0530

    Add files via upload

---
+++ move_fily.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+
+from_dir= "c:/Users/Medhini/Downloads"
+to_dir= "c:/Users/Medhini/OneDrive/Desktop/documnet_files"
+
+list_of_files=os.listdir(from_dir)
+#print(list_of_files)
+
+for(list_of_files):
+os.path.splitext()
